c5f01bd7a67cd56a2bf3d716ba8b968bc1d1be5e
ViFeduk <viktor2005fedchenkov@mail.ru>
2024-02-11 19:44:09 +0300

    Добавьте файлы проекта.

---
+++ test/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Введите возраст");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Ваше имя {name} и возраст {age}");
+            Console.WriteLine("Введите свой день рождения");
+            var birthdate = Console.ReadLine();
+            Console.WriteLine($"Ваш день рождения {birthdate}");
+        }
+
+        
+    }
+}
